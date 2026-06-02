bc3127bad045936c65a2c5c67bf3f4c8cd7d0453
joaofreitas77 <joaozinpr777@gmail.com>
2026-06-01 21:06:19 -0300

    mudanças no tamanho da pagina

--- config/settings.py
+++ config/settings.py
@@ -141,5 +141,5 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 50,
 }
